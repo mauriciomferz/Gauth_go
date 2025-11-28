206fdddc43fe9a92b4d5feccbd7bfd71735afdc6
Mauricio Alejandro Fernandez <56593140+mauriciomferz@users.noreply.github.com>
2025-11-28 16:02:56 +0100

    Update webhook handlers and test data
    - Modified webhook_handlers.go
    - Updated test anchor receipts data

--- pkg/handlers/webhook_handlers.go
+++ pkg/handlers/webhook_handlers.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/mauriciomferz/Gauth_go/pkg/webhook"
 	"github.com/gorilla/mux"
+	"github.com/mauriciomferz/Gauth_go/pkg/webhook"
 )
 
 // WebhookHandler handles webhook-related HTTP requests
